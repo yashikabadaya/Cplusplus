a92c1c420bfd469dc03a620cecf7a19b6b58c8a7
karan7777 <karanrathore7777@gmail.com>
2018-10-14 06:53:51 +0530

    program for Character to ASCII added

---
+++ Char_to_ASCII/Char_to_ASCII.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char abc='a';
+    cout<<"Enter the character ";
+    cin>>abc;
+    cout<<"ASCII of character "<<abc<<" = "<<(int)abc;
+    return 0;
+}
+
+
+
